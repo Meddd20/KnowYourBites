8da9d99614236b8147a9e4d7b08be407f35975ea
Medhiko Biraja <mbirajabiraja@gmail.com>
2025-09-09 15:46:41 +0800

    Initial Commit

---
+++ KnowYourBites/KnowYourBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowYourBitesApp.swift
+//  KnowYourBites
+//
+//  Created by Medhiko Biraja on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct KnowYourBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
